33db2b82f411e0cea2ba0f14724db162310a8e6c
unique01082 <unique01082@gmail.com>
2021-04-05 17:27:40 +0700

    Updates

---
+++ precache-manifest.00b7625c98f4762c2eb5b00899dff7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbdc9cb1773fd2e112a95f8a39ec753",
+    "url": "/react-instances/index.html"
+  },
+  {
+    "revision": "1e27285ddfa8cdd74d75",
+    "url": "/react-instances/static/css/2.e53444ae.chunk.css"
+  },
+  {
+    "revision": "f4766dac7ed14fb1a128",
+    "url": "/react-instances/static/css/main.c19185a6.chunk.css"
+  },
+  {
+    "revision": "1e27285ddfa8cdd74d75",
+    "url": "/react-instances/static/js/2.0575d942.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-instances/static/js/2.0575d942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4766dac7ed14fb1a128",
+    "url": "/react-instances/static/js/main.9eac69d9.chunk.js"
+  },
+  {
+    "revision": "e433dd092ee0fea6c3ab",
+    "url": "/react-instances/static/js/runtime-main.17689e43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d06efc05739553c1d651556723fb2651.js"
+  "/react-instances/precache-manifest.00b7625c98f4762c2eb5b00899dff7b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-instances/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
