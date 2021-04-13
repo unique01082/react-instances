647cceeaf9fcc7ff51a39b5a11c724dc4f6c634d
unique01082 <>
2021-04-13 19:18:13 +0700

    fix: fix withHookManage error on runtime

--- src/hooks/withHookManage.js
+++ src/hooks/withHookManage.js
@@ -1,22 +1,13 @@
-import { useEffect } from 'react'
-import { manageInstances, observable } from '..'
+import {
+  manageInstances,
+  observable,
+  withHookInstanceManage,
+  withHookObserversNotify
+} from '..'
 
 export default function withHookManage(hook) {
   manageInstances(hook)
   observable(hook)
 
-  return new Proxy(hook, {
-    get: Reflect.get,
-    set: Reflect.set,
-    apply(target, thisArgument, [name, ...args]) {
-      const result = Reflect.apply(target, thisArgument, args)
-
-      hook.addInstance(name, result)
-      useEffect(() => {
-        hook.getObservers(name).forEach((watcher) => watcher(result))
-      }, [result])
-      useEffect(() => () => hook.removeInstance(name), [])
-      return result
-    }
-  })
+  return withHookInstanceManage(withHookObserversNotify(hook))
 }
