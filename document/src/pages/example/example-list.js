53a75f5a713e716f5aed7eb9bad894e71ed7c70f
unique01082 <>
2021-04-17 13:07:50 +0700

    feat: update example links status

--- document/src/pages/example/example-list.js
+++ document/src/pages/example/example-list.js
@@ -1,51 +1,51 @@
 export default [
   {
-    name: '[DONE] Interactive with class component',
+    name: 'Interactive with class component',
     path: '/interactive-class-component',
     codeSandboxId: 'interactive-class-component'
   },
   {
-    name: '[DONE] Interactive with function component',
+    name: 'Interactive with function component',
     path: '/interactive-function-component',
     codeSandboxId: 'interactive-function-component'
   },
   {
-    name: '[DONE] Interactive with hook',
+    name: 'Interactive with hook',
     path: '/interactive-hook',
     codeSandboxId: 'interactive-hook'
   },
   {
-    name: '[DONE] Interactive with hook using shortcut',
+    name: 'Interactive with hook using shortcut',
     path: '/interactive-hook-shortcut',
     codeSandboxId: 'interactive-hook-shortcut'
   },
   {
-    name: '[TODO] Observer function component',
+    name: 'Observer function component',
     path: '/observer-function-component',
     codeSandboxId: 'observer-function-component'
   },
   {
-    name: '[TODO] Observer hook',
+    name: 'Observer hook',
     path: '/observer-hook',
     codeSandboxId: 'observer-hook'
   },
   {
-    name: '[TODO] Observer hook using shortcut',
+    name: 'Observer hook using shortcut',
     path: '/observer-hook-shortcut',
     codeSandboxId: 'observer-hook-shortcut'
   },
   {
-    name: '[TODO] Manage function component',
+    name: 'Manage function component',
     path: '/manage-function-component',
     codeSandboxId: 'manage-function-component'
   },
   {
-    name: '[TODO] Manage hook',
+    name: 'Manage hook',
     path: '/manage-hook',
     codeSandboxId: 'manage-hook'
   },
   {
-    name: '[TODO] Manage hook using shortcut',
+    name: 'Manage hook using shortcut',
     path: '/manage-hook-shortcut',
     codeSandboxId: 'manage-hook-shortcut'
   }
